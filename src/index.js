6e6658a7b8d15a19f4752cacf74bf17b8ba3816b
Tushar Mohan <mohan.tushar>
2019-08-03 14:55:07 +0530

    dev: adds linter and packages after ejecting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './screens/App';
 
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById('root'));
